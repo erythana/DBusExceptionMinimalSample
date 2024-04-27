482d199bb5f7db85c51ff02a93af438213dec90d
Christian Berger <c.berger18@gmail.com>
2024-04-27 14:45:07 +0200

    Initial commit

--- DBusExceptionMinimalSample/ViewModels/MainWindowViewModel.cs
+++ DBusExceptionMinimalSample/ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,22 @@
-﻿namespace DBusExceptionMinimalSample.ViewModels;
+﻿using System.Collections.Generic;
+using System.Linq;
+using ReactiveUI;
+
+namespace DBusExceptionMinimalSample.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
-#pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
-#pragma warning restore CA1822 // Mark members as static
+    public MainWindowViewModel()
+    {
+        Items = Enumerable.Range(0, 10).Select(i => new Test() { TestString = "Value" + i });
+        this.RaisePropertyChanged(nameof(Items));
+    }
+    
+    public IEnumerable<Test> Items { get; set; }
 }
+
+public class Test
+{
+    public string TestString { get; set; }
+}
+
